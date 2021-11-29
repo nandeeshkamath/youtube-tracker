c5a3360240d3dfd321a02091574b14e68a34b4b2
Rachana Shetty <rachana.shetty@robosoftin.com>
2021-11-29 18:28:05 +0530

    Updated base code

--- src/main/java/com/youtube/tracker/TrackerApplication.java
+++ src/main/java/com/youtube/tracker/TrackerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YouTubeTrackerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(YouTubeTrackerApplication.class, args);
-    }
+public class TrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerApplication.class, args);
+	}
+
 }
---
+++ src/test/java/com/youtube/tracker/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.youtube.tracker;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TrackerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
