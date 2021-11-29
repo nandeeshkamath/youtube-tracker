bc0895b3fd6aaa08b12dc017c38e81432bbd5e8b
Rachana Shetty <rachana.shetty@robosoftin.com>
2021-11-29 17:14:28 +0530

    Added main

---
+++ src/main/java/com/youtube/tracker/YouTubeTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.youtube.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YouTubeTrackerApplication.class, args);
+    }
+}
