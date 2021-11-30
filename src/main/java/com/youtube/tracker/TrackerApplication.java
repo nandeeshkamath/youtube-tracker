2b7984eabbb67bb1aa80f24941a16a81567cb59c
Rachana-Shetty011 <95215576+Rachana-Shetty011@users.noreply.github.com>
2021-11-30 16:39:32 +0530

    Base code (#6)

---
+++ src/main/java/com/youtube/tracker/TrackerApplication.java
@@ -0,0 +1,13 @@
+package com.youtube.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerApplication.class, args);
+	}
+
+}
