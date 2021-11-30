1161d1c628d7292be15304bc5aeb12b0047b4cee
Rachana Shetty <rachana.shetty@robosoftin.com>
2021-11-30 19:19:22 +0530

    Changed http method

--- src/main/java/com/youtube/tracker/controller/TrackerController.java
+++ src/main/java/com/youtube/tracker/controller/TrackerController.java
@@ -2,12 +2,12 @@ package com.youtube.tracker.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrackerController {
-    @GetMapping("/hello")
+    @PostMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.status(HttpStatus.OK).body("HELLO WELCOME TO YOUTUBE TRACKER");
     }
