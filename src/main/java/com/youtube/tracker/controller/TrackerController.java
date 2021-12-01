a1b2ec92a3c67a53b2286c4c186649a0f1adecb6
nandeeshkamath <58938846+nandeeshkamath@users.noreply.github.com>
2021-12-01 15:34:44 +0530

    Add action for deployment (#16)

--- src/main/java/com/youtube/tracker/controller/TrackerController.java
+++ src/main/java/com/youtube/tracker/controller/TrackerController.java
@@ -2,13 +2,15 @@ package com.youtube.tracker.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrackerController {
-    @PostMapping("/hello")
-    public ResponseEntity<String> hello() {
+
+    @GetMapping("/")
+    public ResponseEntity<String> entryPoint() {
         return ResponseEntity.status(HttpStatus.OK).body("HELLO WELCOME TO YOUTUBE TRACKER");
     }
 }
