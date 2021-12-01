dece6bebd0ccf875ce60a1c094155c71ed7c3f51
Rachana Shetty <rachana.shetty@robosoftin.com>
2021-12-01 17:06:32 +0530

    Merge branch 'main' of https://github.com/nandeeshkamath/youtube-tracker into added_api_key
     Conflicts:
    	src/main/resources/application.properties

--- src/main/java/com/youtube/tracker/controller/TrackerController.java
+++ src/main/java/com/youtube/tracker/controller/TrackerController.java
@@ -1,14 +1,22 @@
 package com.youtube.tracker.controller;
 
+import com.youtube.tracker.feign.YoutubeTrackerFeign;
 import com.youtube.tracker.models.ResponseWrapper;
+import com.youtube.tracker.models.YoutubeSearchResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotBlank;
+
 @RestController
 public class TrackerController {
+    @Autowired
+    YoutubeTrackerFeign youtubeTrackerFeign;
 
     @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
@@ -20,5 +28,13 @@ public class TrackerController {
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseWrapper track() {
         return ResponseWrapper.success();
+    }
+
+    @GetMapping("/search")
+    public ResponseWrapper<YoutubeSearchResponse> getChannelDetails(
+            @RequestParam @NotBlank String part,
+            @RequestParam @NotBlank String key,
+            @RequestParam @NotBlank String id) {
+        return ResponseWrapper.success(youtubeTrackerFeign.getChannels(part, id, key).getBody());
     }
 }
