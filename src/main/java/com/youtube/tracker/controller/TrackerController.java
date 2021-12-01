cdd394328d08320ad141b9b37012191412fb5856
nandeeshkamath <nandeeshkamath99@gmail.com>
2021-12-01 16:51:03 +0530

    Remove unused import

--- src/main/java/com/youtube/tracker/controller/TrackerController.java
+++ src/main/java/com/youtube/tracker/controller/TrackerController.java
@@ -2,7 +2,6 @@ package com.youtube.tracker.controller;
 
 import com.youtube.tracker.models.ResponseWrapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
