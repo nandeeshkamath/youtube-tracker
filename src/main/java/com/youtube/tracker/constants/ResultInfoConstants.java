1358447beb9eac6392c925b2c4998b504a33d04f
nandeeshkamath <58938846+nandeeshkamath@users.noreply.github.com>
2021-12-01 16:59:29 +0530

    Add POST Api controller (#24)

---
+++ src/main/java/com/youtube/tracker/constants/ResultInfoConstants.java
@@ -0,0 +1,7 @@
+package com.youtube.tracker.constants;
+
+import com.youtube.tracker.models.ResultInfo;
+
+public class ResultInfoConstants {
+    public static ResultInfo SUCCESS = new ResultInfo("000", "SUCCESS");
+}
--- src/main/java/com/youtube/tracker/controller/TrackerController.java
+++ src/main/java/com/youtube/tracker/controller/TrackerController.java
@@ -1,16 +1,24 @@
 package com.youtube.tracker.controller;
 
+import com.youtube.tracker.models.ResponseWrapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrackerController {
 
     @GetMapping("/")
-    public ResponseEntity<String> entryPoint() {
-        return ResponseEntity.status(HttpStatus.OK).body("HELLO WELCOME TO YOUTUBE TRACKER");
+    @ResponseStatus(HttpStatus.OK)
+    public String entryPoint() {
+        return "HELLO WELCOME TO YOUTUBE TRACKER";
+    }
+
+    @PostMapping("/track")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseWrapper track() {
+        return ResponseWrapper.success();
     }
 }
