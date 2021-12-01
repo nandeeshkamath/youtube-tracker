5d034873dbdee87f2c4b08bb27a6645eeebaeabf
nandeeshkamath <nandeeshkamath99@gmail.com>
2021-12-01 12:22:53 +0530

    Add entry point

--- src/main/java/com/youtube/tracker/controller/TrackerController.java
+++ src/main/java/com/youtube/tracker/controller/TrackerController.java
@@ -2,11 +2,18 @@ package com.youtube.tracker.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrackerController {
+
+    @GetMapping("/")
+    public String entryPoint() {
+        return "Hello world";
+    }
+
     @PostMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.status(HttpStatus.OK).body("HELLO WELCOME TO YOUTUBE TRACKER");
