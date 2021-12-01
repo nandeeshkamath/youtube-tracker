8d714d9fa2c74555e21036ad78a00841d2ef1816
Rachana-Shetty011 <95215576+Rachana-Shetty011@users.noreply.github.com>
2021-12-01 17:23:58 +0530

    Search api (#27)

--- src/main/java/com/youtube/tracker/feign/TelegramBotFeign.java
+++ src/main/java/com/youtube/tracker/feign/TelegramBotFeign.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 // TODO: Add Configuration for server/client errors
-@FeignClient(value = "telegram-bot-feign", url = "{telegram-api.base-url}")
+@FeignClient(value = "telegram-bot-feign", url = "${telegram-api.base-url}")
 public interface TelegramBotFeign {
 
     @GetMapping("/sendMessage")
