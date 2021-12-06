3af8f3374e78ec1276e495191c8fd60bf4b3ee33
Rachana Shetty <rachana.shetty@robosoftin.com>
2021-12-06 13:38:38 +0530

    Implemented service layer.

---
+++ src/main/java/com/youtube/tracker/service/YouTubeTrackerService.java
@@ -0,0 +1,22 @@
+package com.youtube.tracker.service;
+
+import com.youtube.tracker.feign.YoutubeTrackerFeign;
+import com.youtube.tracker.models.YoutubeSearchResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.NotBlank;
+
+@Service
+@RequiredArgsConstructor
+public class YouTubeTrackerService {
+    private final YoutubeTrackerFeign feign;
+    @Value("${youtube.api-key}")
+    private String apiKey;
+
+    public ResponseEntity<YoutubeSearchResponse> getChannels(@NotBlank String part, @NotBlank String id) {
+        return feign.getChannels(part, id, apiKey);
+    }
+}
