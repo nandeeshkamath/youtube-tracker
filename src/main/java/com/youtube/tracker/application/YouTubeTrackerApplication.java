facbe2676e16843c5372c87d4cf29e9942f9a705
Rachana Shetty <rachana.shetty@robosoftin.com>
2021-12-06 19:20:59 +0530

    Base application layer

---
+++ src/main/java/com/youtube/tracker/application/YouTubeTrackerApplication.java
@@ -0,0 +1,23 @@
+package com.youtube.tracker.application;
+
+import com.youtube.tracker.service.TelegramBotService;
+import com.youtube.tracker.service.YouTubeTrackerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class YouTubeTrackerApplication {
+    @Value("#{'${youtube.api-channel-ids}'.split(',')}")
+    private List<String> channels;
+
+    private final TelegramBotService telegramBotService;
+    private final YouTubeTrackerService youTubeTrackerService;
+
+    private void getChannels(){
+        //todo
+    }
+}
