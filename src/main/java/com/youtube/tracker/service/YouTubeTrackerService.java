df660de9f9012f387c5ff494c2d15f19d0a933f5
Rachana Shetty <rachana.shetty@robosoftin.com>
2021-12-06 18:09:34 +0530

    Added channel ids

--- src/main/java/com/youtube/tracker/service/YouTubeTrackerService.java
+++ src/main/java/com/youtube/tracker/service/YouTubeTrackerService.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +16,9 @@ public class YouTubeTrackerService {
     private final YoutubeTrackerFeign feign;
     @Value("${youtube.api-key}")
     private String apiKey;
+
+    @Value("#{'${youtube.api-channel-ids}'.split(',')}")
+    private List<String> channelId;
 
     public ResponseEntity<YoutubeSearchResponse> getChannels(@NotBlank String part, @NotBlank String channelId, @NotBlank String keyword) {
         return feign.getChannels(part, channelId, apiKey, keyword);
