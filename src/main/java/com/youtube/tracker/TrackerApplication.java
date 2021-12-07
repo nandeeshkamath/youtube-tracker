b17499b7a5195192730efc7dec821698341287ad
Rachana Shetty <rachana.shetty@robosoftin.com>
2021-12-07 13:01:49 +0530

    Minor change

--- src/main/java/com/youtube/tracker/TrackerApplication.java
+++ src/main/java/com/youtube/tracker/TrackerApplication.java
@@ -4,14 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import java.time.Instant;
-import java.time.format.DateTimeFormatter;
-
 @SpringBootApplication
 @EnableFeignClients
 public class TrackerApplication {
-	public static void main(String[] args) {
-		System.out.println("timeeee  "+DateTimeFormatter.ISO_INSTANT.format(Instant.now()));
-		SpringApplication.run(TrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrackerApplication.class, args);
+    }
 }
