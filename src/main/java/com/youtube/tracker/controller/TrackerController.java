cabd8f2222ef5de8b936f68cfc3aca835cf07225
Rachana-Shetty011 <95215576+Rachana-Shetty011@users.noreply.github.com>
2021-12-07 12:57:28 +0530

    Post api (#48)

--- src/main/java/com/youtube/tracker/controller/TrackerController.java
+++ src/main/java/com/youtube/tracker/controller/TrackerController.java
@@ -1,6 +1,8 @@
 package com.youtube.tracker.controller;
 
+import com.youtube.tracker.application.YouTubeTrackerApplication;
 import com.youtube.tracker.models.ResponseWrapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class TrackerController {
+    private final YouTubeTrackerApplication application;
 
     @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
@@ -19,6 +23,7 @@ public class TrackerController {
     @PostMapping("/track")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseWrapper track() {
+        application.trackChannels();
         return ResponseWrapper.success();
     }
 }
