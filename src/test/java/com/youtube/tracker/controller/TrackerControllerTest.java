af9e19eb6472ca31cb6135de5ffb5805af98a108
Nandeesh Kamath <58938846+nandeeshkamath@users.noreply.github.com>
2022-01-22 16:57:40 +0530

    Move values from properties to request body (#57)

---
+++ src/test/java/com/youtube/tracker/controller/TrackerControllerTest.java
@@ -0,0 +1,9 @@
+//package com.youtube.tracker.controller;
+//
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//public class TrackerControllerTest {
+//}
