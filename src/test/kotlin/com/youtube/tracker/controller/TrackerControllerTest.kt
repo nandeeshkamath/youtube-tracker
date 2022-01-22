96b3a7d2edfcddeec4c69a7a7c58c59de49cb4ff
nandeeshkamath <nandeeshkamath99@gmail.com>
2022-01-22 19:44:06 +0530

    Ignore test

--- src/test/kotlin/com/youtube/tracker/controller/TrackerControllerTest.kt
+++ src/test/kotlin/com/youtube/tracker/controller/TrackerControllerTest.kt
@@ -1,8 +1,8 @@
-package com.youtube.tracker.controller
-
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-
-@SpringBootTest
-@AutoConfigureMockMvc
-class TrackerControllerTest 
+//package com.youtube.tracker.controller
+//
+//import org.springframework.boot.test.context.SpringBootTest
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//class TrackerControllerTest
