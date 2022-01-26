d7e59b0814fd5abe2197e0e02d7d02674cb523f1
Nandeesh Kamath <58938846+nandeeshkamath@users.noreply.github.com>
2022-01-26 15:00:02 +0530

    Update README (#75)
    #60

--- src/main/kotlin/com/youtube/tracker/models/request/TrackerRequest.kt
+++ src/main/kotlin/com/youtube/tracker/models/request/TrackerRequest.kt
@@ -1,13 +1,11 @@
 package com.youtube.tracker.models.request
 
-import com.youtube.tracker.enums.PartType
 import com.youtube.tracker.enums.ResultType
 import javax.validation.constraints.NotEmpty
 
 data class TrackerRequest (
     val keywords: @NotEmpty Set<String>,
     val interval: Long?,
-    val type: PartType = PartType.SNIPPET,
     val resultType: ResultType = ResultType.VIDEO,
     val channels: Set<String> = emptySet(),
     val targetChannel: String?,
