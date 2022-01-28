e18455363ce6bc6f61d1052c8856cd1c17f74a37
Nandeesh Kamath <58938846+nandeeshkamath@users.noreply.github.com>
2022-01-28 09:37:32 +0530

    Bugfix: DateTimeFormatter and keywords seperator (#84)

--- src/main/kotlin/com/youtube/tracker/util/Formatters.kt
+++ src/main/kotlin/com/youtube/tracker/util/Formatters.kt
@@ -1,7 +1,9 @@
 package com.youtube.tracker.util
 
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 object Formatters {
-    val INSTANT_ISO_FORMATTER: DateTimeFormatter = DateTimeFormatter.ISO_INSTANT
+    val INSTANT_ISO_FORMATTER: DateTimeFormatter =
+        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'").withZone(ZoneId.of("UTC"))
 }
